447a94a65f6f8e38e62e52b9f37a689a7a3b71ed
Chen <user@use1r-PC>
2017-01-06 14:24:32 +0800

    Change configurations and demos

--- SpringMvc/src/main/java/architect/example1/controller/AnyController1.java
+++ SpringMvc/src/main/java/architect/example1/controller/AnyController1.java
@@ -4,16 +4,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import architect.example1.entity.AnyEntity1;
+import architect.example1.dto.AnyDto;
 
-@RestController
+@RestController// Combines @Controller and @ResponseBody
 public class AnyController1 {
+	// Ctrl click the annotations for more information.
 	@RequestMapping(value = "/example1/{anyValue}", method = RequestMethod.POST)
-	public void anyMethod(@RequestBody AnyEntity1 anyEntity1, 
-			@PathVariable("anyValue") String anyValue) {
-		System.out.println(anyEntity1);
-		System.out.println(anyValue);
+	public void anyMethod(@PathVariable("anyPathVariable") String anyPathVariable,
+			@RequestParam("anyRequestParam") String anyRequestParam,
+			@RequestBody AnyDto anyDto) {
+		System.out.println(anyPathVariable);
+		System.out.println(anyDto);
 	}
 }
---
+++ SpringMvc/src/main/java/architect/example1/dto/AnyDto.java
@@ -0,0 +1,8 @@
+package architect.example1.dto;
+
+import lombok.Data;
+
+@Data
+public class AnyDto {
+	private String name;
+}
